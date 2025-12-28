440d65bd6db064d66ae28197c18c8d2d851fe6aa
crewcrew3 <143967753+crewcrew3@users.noreply.github.com>
2025-12-29 02:47:04 +0300

    Project dependences configurated

---
+++ GloriaArtis/app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.plugin)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+}
+
+android {
+    namespace = "ru.itis.gloriaartis"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "ru.itis.gloriaartis"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    //androidx
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    //compose
+    implementation(libs.bundles.compose.deps)
+    implementation(platform(libs.androidx.compose.bom))
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //DI
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+    //implementation(libs.androidx.hilt.nav.compose)
+
+    //навигация
+    implementation(libs.bundles.nav.deps)
+}
